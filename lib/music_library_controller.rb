58f012327a05d95b91831451f10413d3a0c46008
Laura Felce <crepu83@yahoo.it>
2019-01-11 10:29:27 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -49,7 +49,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_artist
-  input == list artist 
+  input == "list artist"
   puts "Please enter the name of an artist:"
   input = gets.chomp
   end
